4e0eb1c5d1cc095d35a63474f5d63a843d574441
Pierre Klein <pierre@klein.red>
2023-04-13 01:18:32 +0200

    nvim fugitive keymaps; yank highlighting; rm unused keymap; update lsp.lua

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -19,8 +19,21 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-Space>'] = cmp.mapping.complete(),
 })
 
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
 lsp.setup_nvim_cmp({
 	mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
 })
 
 lsp.on_attach(function(client, bufnr)
@@ -42,3 +55,7 @@ end)
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
