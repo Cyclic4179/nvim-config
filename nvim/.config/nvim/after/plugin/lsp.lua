e33e42b01dfd14663d5cc211db817dfe92295c59
Pierre Klein <pierre@klein.red>
2023-05-19 03:28:21 +0200

    stuff

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -3,6 +3,10 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
+    'clangd',
+    'dockerls',
+    'docker_compose_language_service',
+    'texlab',
 	'tsserver',
 	'eslint',
 	'rust_analyzer',
