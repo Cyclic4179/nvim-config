83217f22a6edb5c3d6ac5670ef90c2fbc58a1688
Pierre Klein <pierre@klein.red>
2023-09-17 01:42:42 +0200

    use indent of 2 in yaml files

--- nvim/.config/nvim/lua/pierre/init.lua
+++ nvim/.config/nvim/lua/pierre/init.lua
@@ -25,17 +25,6 @@ autocmd({"BufWritePre"}, {
     command = "%s/\\s\\+$//e",
 })
 
--- yaml indentation fix
-autocmd("FileType", {
-    pattern = "yaml",
-    callback = function()
-        vim.opt_local.tabstop = 4
-        vim.opt_local.softtabstop = 4
-        vim.opt_local.shiftwidth = 4
-        vim.cmd("setlocal indentkeys-=0#")
-    end,
-})
-
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
