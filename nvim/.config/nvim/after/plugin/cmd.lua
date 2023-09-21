02b737d06dbf55a5b8b84b9352d5659dca3c16ce
Pierre Klein <pierre@klein.red>
2023-09-21 13:24:19 +0200

    split lsp and cmd lua files

---
+++ nvim/.config/nvim/after/plugin/cmd.lua
@@ -0,0 +1,94 @@
+local lspkind = require('lspkind')
+
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+    },
+
+    mapping = {
+        -- to test
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-e>'] = cmp.mapping.close(),
+
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({
+            select = true,
+            behavior = cmp.ConfirmBehavior.Insert
+        }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- not use tab
+        ['<Tab>'] = nil,
+        ['<S-Tab>'] = nil
+    },
+
+    sources = cmp.config.sources({
+        { name = 'calc' },
+        { name = 'nvim_lua' },
+        { name = 'nvim_lsp' },
+        -- { name = 'vsnip' }, -- For vsnip users.
+        { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, {
+        { name = 'path' },
+        { name = 'buffer' },
+    }),
+
+    formatting = {
+        -- Youtube: How to set up nice formatting for your sources.
+        format = lspkind.cmp_format {
+            with_text = true,
+            menu = {
+                buffer = "[buf]",
+                nvim_lsp = "[LSP]",
+                nvim_lua = "[api]",
+                path = "[path]",
+                luasnip = "[snip]",
+            },
+        },
+    },
+
+    experimental = {
+        -- Let's play with this for a day or two
+        ghost_text = true,
+    },
+})
+
+
+-- `/` cmdline setup.
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        {
+            name = 'cmdline',
+            option = {
+                ignore_cmds = { 'Man', '!' }
+            }
+        }
+    })
+})
+
+
--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,155 +1,18 @@
---local lsp = require('lsp-zero')
---
---lsp.preset('recommended')
---
---lsp.ensure_installed({
---    'clangd',
---    'dockerls',
---    'docker_compose_language_service',
---    'texlab',
---    'tsserver',
---    'eslint',
---    'rust_analyzer',
---    'pylsp',
---    'lua_ls'
---})
-
-local lspkind = require('lspkind')
-
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-
-cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        end,
-    },
-
-    mapping = {
-        -- to test
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-e>'] = cmp.mapping.close(),
-
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({
-            select = true,
-            behavior = cmp.ConfirmBehavior.Insert
-        }),
-        ['<C-Space>'] = cmp.mapping.complete(),
-
-        -- not use tab
-        ['<Tab>'] = nil,
-        ['<S-Tab>'] = nil
-    },
-
-    sources = cmp.config.sources({
-        { name = 'calc' },
-        { name = 'nvim_lua' },
-        { name = 'nvim_lsp' },
-        -- { name = 'vsnip' }, -- For vsnip users.
-        { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-    }, {
-        { name = 'path' },
-        { name = 'buffer' },
-    }),
-
-    formatting = {
-        -- Youtube: How to set up nice formatting for your sources.
-        format = lspkind.cmp_format {
-            with_text = true,
-            menu = {
-                buffer = "[buf]",
-                nvim_lsp = "[LSP]",
-                nvim_lua = "[api]",
-                path = "[path]",
-                luasnip = "[snip]",
-            },
-        },
-    },
-
-    experimental = {
-        -- Let's play with this for a day or two
-        ghost_text = true,
-    },
-})
-
-
--- `/` cmdline setup.
-cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' }
-    }
-})
-
--- `:` cmdline setup.
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        {
-            name = 'cmdline',
-            option = {
-                ignore_cmds = { 'Man', '!' }
-            }
-        }
-    })
-})
-
-
---lsp.setup_nvim_cmp({
---    mapping = cmp_mappings
---})
-
-
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
-})
-
-lsp.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
-
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-
-end)
-
+-- Setup language servers.
+local lspconfig = require('lspconfig')
 
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-    capabilities = capabilities
-}
+--require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+--    capabilities = capabilities
+--}
 
 --require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
-require('lspconfig').pylsp.setup {
+lspconfig.pylsp.setup {
+    capabilities = capabilities,
     settings = {
         pylsp = {
             plugins = {
@@ -167,6 +30,137 @@ require('lspconfig').pylsp.setup {
         }
     }
 }
+
+lspconfig.pyright.setup { capabilities = capabilities }
+lspconfig.tsserver.setup { capabilities = capabilities }
+lspconfig.rust_analyzer.setup {
+    capabilities = capabilities,
+    -- Server-specific settings. See `:help lspconfig-setup`
+    settings = {
+        ['rust-analyzer'] = {},
+    }
+}
+-- TODO for some reason not working
+lspconfig.nil_ls.setup {
+    capabilities = capabilities,
+    autostart = true
+}
+--_ = vim.cmd [[
+--    augroup CmpNix
+--      au!
+--      autocmd Filetype nix lua print("nix file!")
+--    augroup END
+--]]
+lspconfig.clangd.setup { capabilities = capabilities }
+lspconfig.dockerls.setup { capabilities = capabilities }
+lspconfig.texlab.setup { capabilities = capabilities }
+lspconfig.pylsp.setup { capabilities = capabilities }
+lspconfig.lua_ls.setup { capabilities = capabilities }
+
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+--local lsp = require('lsp-zero')
+--
+--lsp.preset('recommended')
+--
+--lsp.ensure_installed({
+--    'clangd',
+--    'dockerls',
+--    'docker_compose_language_service',
+--    'texlab',
+--    'tsserver',
+--    'eslint',
+--    'rust_analyzer',
+--    'pylsp',
+--    'lua_ls'
+--})
+
+--lsp.setup_nvim_cmp({
+--    mapping = cmp_mappings
+--})
+
+
+--lsp.set_preferences({
+--    suggest_lsp_servers = false,
+--    sign_icons = {
+--        error = 'E',
+--        warn = 'W',
+--        hint = 'H',
+--        info = 'I'
+--    }
+--})
+
+local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+--lsp.on_attach(function(client, bufnr)
+--    local opts = {buffer = bufnr, remap = false}
+--
+--    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+--    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+--    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+--    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+--    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+--    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+--    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+--    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+--    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+--    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+--
+--end)
+
 
 --lsp.setup()
 
