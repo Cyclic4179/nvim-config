6df2e23d8281ccfc75ad885f0e989b5cee8d4f11
Pierre Klein <pierre@klein.red>
2023-11-26 17:51:35 +0100

    lsp changes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,7 +3,6 @@ local lspconfig = require('lspconfig')
 
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local home = vim.fn.getenv('HOME')
 
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 --require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
@@ -35,17 +34,19 @@ lspconfig.pylsp.setup {
 lspconfig.ocamllsp.setup {}
 
 --lspconfig.pyright.setup { capabilities = capabilities }
-lspconfig.jdtls.setup {
-    capabilities = capabilities,
-    cmd = { "jdt-language-server", "-data", home .. "/.cache/jdtls/workspace" },
-    root_dir = function(fname)
-        return require("lspconfig").util.root_pattern("pom.xml", "gradle.build", ".git")(fname) or vim.fn.getcwd()
-    end,
-}
+--lspconfig.jdtls.setup {
+--    capabilities = capabilities,
+--    cmd = { "jdt-language-server", "-data", home .. "/.cache/jdtls/workspace" },
+--    root_dir = function(fname)
+--		return require("lspconfig").util.root_pattern("pom.xml", "gradle.build", ".git")(fname) or vim.fn.getcwd()
+--	end,
+--}
+--
 --lspconfig.java_language_server.setup {
 --    capabilities = capabilities,
 --    cmd = { 'java-language-server' }
 --}
+
 lspconfig.tsserver.setup { capabilities = capabilities }
 lspconfig.rust_analyzer.setup {
     capabilities = capabilities,
@@ -115,7 +116,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     --vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    --vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set({"i", "n"}, "<C-h>", vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
     --vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
@@ -125,17 +127,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
     --vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    --vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+    --vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>f', function()
         vim.lsp.buf.format { async = true }
     end, opts)
     --vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
     --vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end,
 })
 
