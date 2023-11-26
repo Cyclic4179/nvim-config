4f753bbbd227f96e759ea93a403ad4e524fbd1f5
Pierre Klein <pierre@klein.red>
2023-11-26 17:49:45 +0100

    java main func snippet

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -10,34 +10,34 @@ local func = ls.function_node
 local choice = ls.choice_node
 local dynamicn = ls.dynamic_node
 
-local date = function() return {vim.fn.date('%Y-%m-%d')} end
+--local date = function() return {vim.fn.date('%Y-%m-%d')} end
 
 ls.add_snippets(nil, {
     all = {
-        snip({
-            trig = "date",
-            namr = "Date",
-            dscr = "Date in the form of YYYY-MM-DD",
-        }, {
-            func(date, {}),
-        }),
-        snip({
-            trig = "meta",
-            namr = "Metadata",
-            dscr = "Yaml metadata format for markdown"
-        },
-        {
-            text({"---",
-            "title: "}), insert(1, "note_title"), text({"",
-            "author: "}), insert(2, "author"), text({"",
-            "date: "}), func(date, {}), text({"",
-            "categories: ["}), insert(3, ""), text({"]",
-            "lastmod: "}), func(date, {}), text({"",
-            "tags: ["}), insert(4), text({"]",
-            "comments: true",
-            "---", ""}),
-            insert(0)
-        })
+        --snip({
+        --    trig = "date",
+        --    namr = "Date",
+        --    dscr = "Date in the form of YYYY-MM-DD",
+        --}, {
+        --    func(date, {}),
+        --}),
+        --snip({
+        --    trig = "meta",
+        --    namr = "Metadata",
+        --    dscr = "Yaml metadata format for markdown"
+        --},
+        --{
+        --    text({"---",
+        --    "title: "}), insert(1, "note_title"), text({"",
+        --    "author: "}), insert(2, "author"), text({"",
+        --    "date: "}), func(date, {}), text({"",
+        --    "categories: ["}), insert(3, ""), text({"]",
+        --    "lastmod: "}), func(date, {}), text({"",
+        --    "tags: ["}), insert(4), text({"]",
+        --    "comments: true",
+        --    "---", ""}),
+        --    insert(0)
+        --})
     },
     java = {
         snip({
