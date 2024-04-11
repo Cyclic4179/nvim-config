bc4cc63714e4758b6e9432fed6ef07e857b099b3
Pierre Klein <pierre@klein.red>
2024-04-12 01:19:45 +0200

    asdf

--- flake.nix
+++ flake.nix
@@ -1,24 +1,42 @@
 {
   description = "NeoVim config";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nix2vim.url = "github:gytis-ivaskevicius/nix2vim";
+  inputs.nix2vim.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nix2vim.inputs.flake-utils.follows = "flake-utils";
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
     {
+      self,
+      nixpkgs,
+      flake-utils,
+      nix2vim,
+    }:
+    #{
+    #  packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    #};
 
-      #hmModules.nvim = import ./nvim-config.nix;
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix2vim.overlay ];
+        };
+      in
+      {
+        #packages.default = pkgs.nix2vimDemo;
+        #apps = import ./apps.nix { inherit pkgs; utils = flake-utils.lib; };
+        #checks = import ./checks { inherit pkgs dsl; check-utils = import ./check-utils.nix; };
 
-      #packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        #hmModules.nvim = import ./nvim-config.nix;
 
-      packages.x86_64-linux.default =
-        (x: builtins.trace "testing" nixpkgs.legacyPackages.x86_64-linux.hello)
-          3;
+        packages.default = pkgs.hello; # pkgs.neovimBuilder { };
+        #  (x: builtins.trace "testing" nixpkgs.legacyPackages.x86_64-linux.hello)
+        #    3;
+      }
+    )
+    // {
+      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
     };
 }
