d7163ff1af41fcd578397369cfd1a237efa672ad
Pierre Klein <pierre@klein.red>
2024-08-02 14:41:18 +0200

    telescope remaps, rm test.nix

--- config/after/plugin/telescope.lua
+++ config/after/plugin/telescope.lua
@@ -3,5 +3,11 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-P>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+
+vim.keymap.set('n', '<leader>pt', function ()
+    builtin.grep_string({ search = "TODO" })
+end, {})
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
