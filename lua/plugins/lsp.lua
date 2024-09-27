5623104b350742392d85d66b0875a6d5b9255288
Pierre Klein <pierre@klein.red>
2024-09-27 17:16:01 +0200

    fix: border

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -165,6 +165,25 @@ return {
             vim.diagnostic.config({
                 virtual_text = true,
             })
+
+            -- see https://vi.stackexchange.com/questions/39074/user-borders-around-lsp-floating-windows
+            local _border = "rounded" -- none single double rounded solid shadow
+
+            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+                border = _border,
+            })
+
+            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+                border = _border,
+            })
+
+            vim.diagnostic.config({
+                float = { border = _border },
+            })
+
+            require("lspconfig.ui.windows").default_options = {
+                border = _border,
+            }
         end,
     },
 }
