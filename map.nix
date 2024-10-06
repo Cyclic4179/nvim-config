7796e5cb4c0a8a6c6b0eb79245fed30914741d81
Pierre Klein <pierre@klein.red>
2024-10-06 18:25:58 +0200

    add lib.nix

--- map.nix
+++ map.nix
@@ -19,6 +19,7 @@
   * }
 */
 let
+  inherit (import ./lib.nix args) toPrettyString;
   recMapNix =
     let
       merge =
@@ -133,27 +134,6 @@ let
       extraPackages = lib.unique res.extraPackages;
       vimPlugins = lib.unique res.vimPlugins;
     };
-
-  toPrettyString =
-    x:
-    if builtins.isNull x then
-      "null"
-    else if builtins.isString x then
-      ''"${lib.escape [ "\"" ] x}"''
-    else if builtins.isBool x then
-      if x then "true" else false
-    else if
-      builtins.isInt x || builtins.isFloat x || builtins.isPath x || x ? outPath || x ? __toString
-    then
-      toString x
-    else if builtins.isList x then
-      "[ ${builtins.foldl' (acc: v: acc + toPrettyString v + " ") "" x}]"
-    else if builtins.isAttrs x then
-      "{ ${
-        builtins.foldl' (acc: v: acc + "${v} = ${toPrettyString x.${v}}; ") "" (builtins.attrNames x)
-      }}"
-    else
-      "«lambda»";
 
 in
 wrapper
