0fc37971b02221997bab39596b1d4ec60b12d9f0
Pierre Klein <pierre@klein.red>
2025-07-09 17:19:02 +0200

    julials

--- config/migration/global-vars.nix
+++ config/migration/global-vars.nix
@@ -4,6 +4,8 @@
   inputs,
 }:
 
+# use like vim.g.MY_VAR
+
 {
   JAVA_SE_17 = "${pkgs.jdk17}/lib/openjdk";
   # https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml
@@ -36,4 +38,36 @@
       paths = pkgs.vimPlugins.nvim-treesitter.withAllGrammars.dependencies;
     }
   );
+  JULIA_WITH_LS_PATH = lib.getExe (pkgs.julia.withPackages [ "LanguageServer" ]);
+  JULIA_LS_START_FILE =
+    builtins.toFile "StartLanguageServer.jl" # julia
+      ''
+        using LanguageServer
+
+        depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
+
+        project_path = let
+            dirname(something(
+                ## 1. Finds an explicitly set project (JULIA_PROJECT)
+                Base.load_path_expand((
+                    p = get(ENV, "JULIA_PROJECT", nothing);
+                    p === nothing ? nothing : isempty(p) ? nothing : p
+                )),
+                ## 2. Look for a Project.toml file in the current working directory,
+                ##    or parent directories, with $HOME as an upper boundary
+                Base.current_project(),
+                ## 3. First entry in the load path
+                get(Base.load_path(), 1, nothing),
+                ## 4. Fallback to default global environment,
+                ##    this is more or less unreachable
+                Base.load_path_expand("@v#.#"),
+            ))
+        end
+
+        @info "Running language server" VERSION pwd() project_path depot_path
+
+        server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)
+        server.runlinter = true
+        run(server)
+      '';
 }
